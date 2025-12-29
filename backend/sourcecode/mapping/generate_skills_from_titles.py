ce7a016837dbe67acdc443254b4f6f7a09109d0f
JustinYuanZe <lehotrongtin123yuanze@gmail.com>
2025-12-30 01:40:19 +0800

    Update generate_skills_from_titles.py

--- backend/sourcecode/mapping/generate_skills_from_titles.py
+++ backend/sourcecode/mapping/generate_skills_from_titles.py
@@ -5,7 +5,7 @@ import pandas as pd
 from groq import Groq
 
 # --- CONFIGURATION ---
-client = Groq(api_key="") 
+client = Groq() 
 
 # Corrected paths based on your file structure
 INPUT_FILE = "data/Raw/JobsDatasetProcessed.csv"
